dbc470ae6ab033b0b12ce26440ce7b314a6bd2e4
FrancielEusebio <2019-0198@unad.edu.do>
2023-02-16 13:35:53 -0400

    create proyect

---
+++ WebApi/RosaFiestaContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi;
+
+public class RosaFiestaContext : DbContext
+{
+    // Aqui se declara tabla
+    public RosaFiestaContext(DbContextOptions<RosaFiestaContext> options) : base(options)
+    {
+        User = Set<UserModel>();
+    }
+
+    // Se va a declarar relaciones de tablas 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder, nameof(modelBuilder));
+        modelBuilder.HasDefaultSchema("rosafiesta");
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.UseIdentityColumns();
+    }
+
+    public DbSet<UserModel> User { get; }
+}
