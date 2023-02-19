56cc841f22a065945adf217542bd93a75fa39b5d
FrancielEusebio <2019-0198@unad.edu.do>
2023-02-19 13:01:29 -0400

    User Controller Get Post

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly RosaFiestaContext _context;  
+    public UserController(RosaFiestaContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("usuarios")]afdsfdsa
+    public async Task<ActionResult<IEnumerable<UserModel>>> GetUsers()
+    {
+        return await _context.User.ToListAsync();
+    }
+
+    [HttpPost("crearUsuario")]
+    public async Task<ActionResult<UserModel>> CreateUser(UserModel user)
+    {
+        _context.User.Add(user);
+        await _context.SaveChangesAsync();
+        return Ok(user);
+    }
+
+
+}
