6c46ad57f9af3f362afa5867a47a4b4b30254d11
FrancielEusebio <2019-0198@unad.edu.do>
2023-02-19 13:05:38 -0400

    CONTROLLER FIX AGAIN

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -17,7 +17,7 @@ public class UserController : ControllerBase
         _context = context;
     }
 
-    [HttpGet("usuarios")]DSADSADSADSADSA
+    [HttpGet("usuarios")]
     public async Task<ActionResult<IEnumerable<UserModel>>> GetUsers()
     {
         return await _context.User.ToListAsync();
