0be725c4e1aba91c4de0b11838de7def17f44f3c
jeudryWepsys <111447813+jeudryWepsys@users.noreply.github.com>
2023-05-01 17:37:09 -0400

    Loggin completed

--- ClientApp/src/app/components/app-component/app.component.ts
+++ ClientApp/src/app/components/app-component/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ClientApp';
+
+  constructor(
+    private router: Router
+  ) {}
+
+  Login() {
+    this.router.navigate(['/authenticate']);
+  }
 }
