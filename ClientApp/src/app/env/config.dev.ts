1ab3c00629ff42e582db03acb5fd6e20c7bbc4ce
jeudryWepsys <111447813+jeudryWepsys@users.noreply.github.com>
2023-05-02 16:20:41 -0400

    added configs for enviroment

---
+++ ClientApp/src/app/env/config.dev.ts
@@ -0,0 +1,6 @@
+import { Config } from "./config";
+
+export const config: Config = {
+  apiURL: 'https://my-api-dev.com',
+  production: false,
+};
---
+++ ClientApp/src/app/env/config.prod.ts
@@ -0,0 +1,6 @@
+import { Config } from "./config";
+
+export const config: Config = {
+  apiURL: 'https://my-api-prod.com',
+  production: true,
+};
