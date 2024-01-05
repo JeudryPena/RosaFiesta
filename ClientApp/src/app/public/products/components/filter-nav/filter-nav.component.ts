e688358ed642c7e32e34488c52c16d41df0b0c29
jeudryWepsys <111447813+jeudryWepsys@users.noreply.github.com>
2024-01-04 22:50:02 -0400

    filter nav

---
+++ ClientApp/src/app/public/products/components/filter-nav/filter-nav.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-filter-nav',
+  templateUrl: './filter-nav.component.html',
+  styleUrl: './filter-nav.component.sass'
+})
+export class FilterNavComponent {
+
+}
