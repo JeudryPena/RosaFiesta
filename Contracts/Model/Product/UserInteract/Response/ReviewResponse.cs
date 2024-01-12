91e66a3618b525d2edbe3354f4a7785515b2cfad
jeudryWepsys <111447813+jeudryWepsys@users.noreply.github.com>
2024-01-11 22:30:02 -0400

    100% working reviews

--- Contracts/Model/Product/UserInteract/Response/ReviewResponse.cs
+++ Contracts/Model/Product/UserInteract/Response/ReviewResponse.cs
@@ -2,7 +2,7 @@
 
 public class ReviewResponse : BaseResponse
 {
-	public Guid Id { get; set; }
+	public int Id { get; set; }
 	public string? Description { get; set; }
 	public float Rating { get; set; }
 	public string? Title { get; set; }
